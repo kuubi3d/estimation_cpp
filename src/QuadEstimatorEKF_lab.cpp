2e08f8d846324d0262ac1ea0191c80e000481d56
kuubi3d <47939652+kuubi3d@users.noreply.github.com>
2021-03-24 22:09:02 -1000

    update

--- src/QuadEstimatorEKF_lab.cpp
+++ src/QuadEstimatorEKF_lab.cpp
@@ -100,12 +100,6 @@ void QuadEstimatorEKF::UpdateFromIMU(V3F accel, V3F gyro)
 /* ~ Udacity C++ Vectors and Iterators
 
 int main ()
-// constructing vectors
-// constructing vectors
-#include <iostream>
-#include <vector>
-
-
 int main ()
 {
   //creating a vector of integers
@@ -115,20 +109,26 @@ int main ()
   
   std::cout<<"vectorInts has "<<vectorInts.size()<<" elements\n";
   
+  std::cout<<"Adding 23 to the vector\n";
+  vectorInts.assign(1,23);
+  for (it = vectorInts.begin(); it != vectorInts.end(); ++it)
+    std::cout<<*it<<" ";
+  
+
   std::cout<<"\n\nAdding four elements to the vector\n";
   //assigning the value 3 to 4 elements of the vector
-  vectorInts.assign(4,3);
+  vectorInts.push_back(24);
+  vectorInts.push_back(25);
+  vectorInts.push_back(26);
+  vectorInts.push_back(27);
   std::cout<<"vectorInts has "<<vectorInts.size()<<" elements\n";
   
   //printing the contents of vectorInts
   std::cout<<"VectorInts has these elements:\n";
   for (it = vectorInts.begin(); it != vectorInts.end(); ++it)
     std::cout<<*it<<" ";
-
+ 
   return 0;
-}
-
-
 }
 
 */
