7fb954003d09e4f18bea8e07f319b06f3a5ac01e
kuubi3d <47939652+kuubi3d@users.noreply.github.com>
2021-03-24 21:57:38 -1000

    update

--- src/QuadEstimatorEKF_lab.cpp
+++ src/QuadEstimatorEKF_lab.cpp
@@ -100,16 +100,35 @@ void QuadEstimatorEKF::UpdateFromIMU(V3F accel, V3F gyro)
 /* ~ Udacity C++ Vectors and Iterators
 
 int main ()
+// constructing vectors
+// constructing vectors
+#include <iostream>
+#include <vector>
+
+
+int main ()
 {
   //creating a vector of integers
   std::vector<int> vectorInts;  
+  //creating an iterator for the vector
+  std::vector<int>::iterator it;
+  
   std::cout<<"vectorInts has "<<vectorInts.size()<<" elements\n";
   
-  //Changing the size of vectorInts to 6
-  vectorInts.resize(6);
-  std::cout<<"\n\nvectorInts now has "<<vectorInts.size()<<" elements\n";
- 
+  std::cout<<"\n\nAdding four elements to the vector\n";
+  //assigning the value 3 to 4 elements of the vector
+  vectorInts.assign(4,3);
+  std::cout<<"vectorInts has "<<vectorInts.size()<<" elements\n";
+  
+  //printing the contents of vectorInts
+  std::cout<<"VectorInts has these elements:\n";
+  for (it = vectorInts.begin(); it != vectorInts.end(); ++it)
+    std::cout<<*it<<" ";
+
   return 0;
+}
+
+
 }
 
 */
