1c513069136bc818deef6501b099830a0c6f0a19
kuubi3d <47939652+kuubi3d@users.noreply.github.com>
2021-03-26 06:47:33 -1000

    update

--- src/elements/ekf_vect_itr.cpp
+++ src/elements/ekf_vect_itr.cpp
@@ -328,3 +328,273 @@ Quatf Ekf::calculate_quaternion() const
   /////////////////////////////// END STUDENT CODE ////////////////////////////
 
   return predictedState;
+}
+
+MatrixXf QuadEstimatorEKF::GetRbgPrime(float roll, float pitch, float yaw)
+{
+  // first, figure out the Rbg_prime
+  MatrixXf RbgPrime(3, 3);
+  RbgPrime.setZero();
+
+  // Return the partial derivative of the Rbg rotation matrix with respect to yaw. We call this RbgPrime.
+  // INPUTS: 
+  //   roll, pitch, yaw: Euler angles at which to calculate RbgPrime
+  //   
+  // OUTPUT:
+  //   return the 3x3 matrix representing the partial derivative at the given point
+
+  // HINTS
+  // - this is just a matter of putting the right sin() and cos() functions in the right place.
+  //   make sure you write clear code and triple-check your math
+  // - You can also do some numerical partial derivatives in a unit test scheme to check 
+  //   that your calculations are reasonable
+
+  ////////////////////////////// BEGIN STUDENT CODE ///////////////////////////
+
+
+  /////////////////////////////// END STUDENT CODE ////////////////////////////
+
+  return RbgPrime;
+}
+
+void QuadEstimatorEKF::Predict(float dt, V3F accel, V3F gyro)
+{
+  // predict the state forward
+  VectorXf newState = PredictState(ekfState, dt, accel, gyro);
+
+  // Predict the current covariance forward by dt using the current accelerations and body rates as input.
+  // INPUTS: 
+  //   dt: time step to predict forward by [s]
+  //   accel: acceleration of the vehicle, in body frame, *not including gravity* [m/s2]
+  //   gyro: body rates of the vehicle, in body frame [rad/s]
+  //   state (member variable): current state (state at the beginning of this prediction)
+  //   
+  // OUTPUT:
+  //   update the member variable cov to the predicted covariance
+
+  // HINTS
+  // - update the covariance matrix cov according to the EKF equation.
+  // 
+  // - you may find the current estimated attitude in variables rollEst, pitchEst, state(6).
+  //
+  // - use the class MatrixXf for matrices. To create a 3x5 matrix A, use MatrixXf A(3,5).
+  //
+  // - the transition model covariance, Q, is loaded up from a parameter file in member variable Q
+  // 
+  // - This is unfortunately a messy step. Try to split this up into clear, manageable steps:
+  //   1) Calculate the necessary helper matrices, building up the transition jacobian
+  //   2) Once all the matrices are there, write the equation to update cov.
+  //
+  // - if you want to transpose a matrix in-place, use A.transposeInPlace(), not A = A.transpose()
+  // 
+
+  // we'll want the partial derivative of the Rbg matrix
+  MatrixXf RbgPrime = GetRbgPrime(rollEst, pitchEst, ekfState(6));
+
+  // we've created an empty Jacobian for you, currently simply set to identity
+  MatrixXf gPrime(QUAD_EKF_NUM_STATES, QUAD_EKF_NUM_STATES);
+  gPrime.setIdentity();
+
+  ////////////////////////////// BEGIN STUDENT CODE ///////////////////////////
+
+
+  /////////////////////////////// END STUDENT CODE ////////////////////////////
+
+  ekfState = newState;
+}
+
+void QuadEstimatorEKF::UpdateFromGPS(V3F pos, V3F vel)
+{
+  VectorXf z(6), zFromX(6);
+  z(0) = pos.x;
+  z(1) = pos.y;
+  z(2) = pos.z;
+  z(3) = vel.x;
+  z(4) = vel.y;
+  z(5) = vel.z;
+
+  MatrixXf hPrime(6, QUAD_EKF_NUM_STATES);
+  hPrime.setZero();
+
+  // GPS UPDATE
+  // Hints: 
+  //  - The GPS measurement covariance is available in member variable R_GPS
+  //  - this is a very simple update
+  ////////////////////////////// BEGIN STUDENT CODE ///////////////////////////
+
+  /////////////////////////////// END STUDENT CODE ////////////////////////////
+
+  Update(z, hPrime, R_GPS, zFromX);
+}
+
+void QuadEstimatorEKF::UpdateFromMag(float magYaw)
+{
+  VectorXf z(1), zFromX(1);
+  z(0) = magYaw;
+
+  MatrixXf hPrime(1, QUAD_EKF_NUM_STATES);
+  hPrime.setZero();
+
+  // MAGNETOMETER UPDATE
+  // Hints: 
+  //  - Your current estimated yaw can be found in the state vector: ekfState(6)
+  //  - Make sure to normalize the difference between your measured and estimated yaw
+  //    (you don't want to update your yaw the long way around the circle)
+  //  - The magnetomer measurement covariance is available in member variable R_Mag
+  ////////////////////////////// BEGIN STUDENT CODE ///////////////////////////
+
+
+  /////////////////////////////// END STUDENT CODE ////////////////////////////
+
+  Update(z, hPrime, R_Mag, zFromX);
+}
+
+// Execute an EKF update step
+// z: measurement
+// H: Jacobian of observation function evaluated at the current estimated state
+// R: observation error model covariance 
+// zFromX: measurement prediction based on current state
+void QuadEstimatorEKF::Update(VectorXf& z, MatrixXf& H, MatrixXf& R, VectorXf& zFromX)
+{
+  assert(z.size() == H.rows());
+  assert(QUAD_EKF_NUM_STATES == H.cols());
+  assert(z.size() == R.rows());
+  assert(z.size() == R.cols());
+  assert(z.size() == zFromX.size());
+
+  MatrixXf toInvert(z.size(), z.size());
+  toInvert = H*ekfCov*H.transpose() + R;
+  MatrixXf K = ekfCov * H.transpose() * toInvert.inverse();
+
+  ekfState = ekfState + K*(z - zFromX);
+
+  MatrixXf eye(QUAD_EKF_NUM_STATES, QUAD_EKF_NUM_STATES);
+  eye.setIdentity();
+
+  ekfCov = (eye - K*H)*ekfCov;
+}
+
+// Calculate the condition number of the EKF ovariance matrix (useful for numerical diagnostics)
+// The condition number provides a measure of how similar the magnitudes of the error metric beliefs 
+// about the different states are. If the magnitudes are very far apart, numerical issues will start to come up.
+float QuadEstimatorEKF::CovConditionNumber() const
+{
+  MatrixXf m(7, 7);
+  for (int i = 0; i < 7; i++)
+  {
+    for (int j = 0; j < 7; j++)
+    {
+      m(i, j) = ekfCov(i, j);
+    }
+  }
+
+  Eigen::JacobiSVD<MatrixXf> svd(m);
+  float cond = svd.singularValues()(0)
+    / svd.singularValues()(svd.singularValues().size() - 1);
+  return cond;
+}
+
+// Access functions for graphing variables
+bool QuadEstimatorEKF::GetData(const string& name, float& ret) const
+{
+  if (name.find_first_of(".") == string::npos) return false;
+  string leftPart = LeftOf(name, '.');
+  string rightPart = RightOf(name, '.');
+
+  if (ToUpper(leftPart) == ToUpper(_name))
+  {
+#define GETTER_HELPER(A,B) if (SLR::ToUpper(rightPart) == SLR::ToUpper(A)){ ret=(B); return true; }
+    GETTER_HELPER("Est.roll", rollEst);
+    GETTER_HELPER("Est.pitch", pitchEst);
+
+    GETTER_HELPER("Est.x", ekfState(0));
+    GETTER_HELPER("Est.y", ekfState(1));
+    GETTER_HELPER("Est.z", ekfState(2));
+    GETTER_HELPER("Est.vx", ekfState(3));
+    GETTER_HELPER("Est.vy", ekfState(4));
+    GETTER_HELPER("Est.vz", ekfState(5));
+    GETTER_HELPER("Est.yaw", ekfState(6));
+
+    GETTER_HELPER("Est.S.x", sqrtf(ekfCov(0, 0)));
+    GETTER_HELPER("Est.S.y", sqrtf(ekfCov(1, 1)));
+    GETTER_HELPER("Est.S.z", sqrtf(ekfCov(2, 2)));
+    GETTER_HELPER("Est.S.vx", sqrtf(ekfCov(3, 3)));
+    GETTER_HELPER("Est.S.vy", sqrtf(ekfCov(4, 4)));
+    GETTER_HELPER("Est.S.vz", sqrtf(ekfCov(5, 5)));
+    GETTER_HELPER("Est.S.yaw", sqrtf(ekfCov(6, 6)));
+
+    // diagnostic variables
+    GETTER_HELPER("Est.D.AccelPitch", accelPitch);
+    GETTER_HELPER("Est.D.AccelRoll", accelRoll);
+
+    GETTER_HELPER("Est.D.ax_g", accelG[0]);
+    GETTER_HELPER("Est.D.ay_g", accelG[1]);
+    GETTER_HELPER("Est.D.az_g", accelG[2]);
+
+    GETTER_HELPER("Est.E.x", trueError(0));
+    GETTER_HELPER("Est.E.y", trueError(1));
+    GETTER_HELPER("Est.E.z", trueError(2));
+    GETTER_HELPER("Est.E.vx", trueError(3));
+    GETTER_HELPER("Est.E.vy", trueError(4));
+    GETTER_HELPER("Est.E.vz", trueError(5));
+    GETTER_HELPER("Est.E.yaw", trueError(6));
+    GETTER_HELPER("Est.E.pitch", pitchErr);
+    GETTER_HELPER("Est.E.roll", rollErr);
+    GETTER_HELPER("Est.E.MaxEuler", maxEuler);
+
+    GETTER_HELPER("Est.E.pos", posErrorMag);
+    GETTER_HELPER("Est.E.vel", velErrorMag);
+
+    GETTER_HELPER("Est.D.covCond", CovConditionNumber());
+#undef GETTER_HELPER
+  }
+  return false;
+};
+
+vector<string> QuadEstimatorEKF::GetFields() const
+{
+  vector<string> ret = BaseQuadEstimator::GetFields();
+  ret.push_back(_name + ".Est.roll");
+  ret.push_back(_name + ".Est.pitch");
+
+  ret.push_back(_name + ".Est.x");
+  ret.push_back(_name + ".Est.y");
+  ret.push_back(_name + ".Est.z");
+  ret.push_back(_name + ".Est.vx");
+  ret.push_back(_name + ".Est.vy");
+  ret.push_back(_name + ".Est.vz");
+  ret.push_back(_name + ".Est.yaw");
+
+  ret.push_back(_name + ".Est.S.x");
+  ret.push_back(_name + ".Est.S.y");
+  ret.push_back(_name + ".Est.S.z");
+  ret.push_back(_name + ".Est.S.vx");
+  ret.push_back(_name + ".Est.S.vy");
+  ret.push_back(_name + ".Est.S.vz");
+  ret.push_back(_name + ".Est.S.yaw");
+
+  ret.push_back(_name + ".Est.E.x");
+  ret.push_back(_name + ".Est.E.y");
+  ret.push_back(_name + ".Est.E.z");
+  ret.push_back(_name + ".Est.E.vx");
+  ret.push_back(_name + ".Est.E.vy");
+  ret.push_back(_name + ".Est.E.vz");
+  ret.push_back(_name + ".Est.E.yaw");
+  ret.push_back(_name + ".Est.E.pitch");
+  ret.push_back(_name + ".Est.E.roll");
+
+  ret.push_back(_name + ".Est.E.pos");
+  ret.push_back(_name + ".Est.E.vel");
+
+  ret.push_back(_name + ".Est.E.maxEuler");
+
+  ret.push_back(_name + ".Est.D.covCond");
+
+  // diagnostic variables
+  ret.push_back(_name + ".Est.D.AccelPitch");
+  ret.push_back(_name + ".Est.D.AccelRoll");
+  ret.push_back(_name + ".Est.D.ax_g");
+  ret.push_back(_name + ".Est.D.ay_g");
+  ret.push_back(_name + ".Est.D.az_g");
+  return ret;
+};
