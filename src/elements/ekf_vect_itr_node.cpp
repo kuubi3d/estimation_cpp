bfca284f7b3323c6887254306c210eb7accc80ac
kuubi3d <47939652+kuubi3d@users.noreply.github.com>
2021-03-26 07:14:14 -1000

    update

---
+++ src/elements/ekf_vect_itr_node.cpp
@@ -0,0 +1,330 @@
+#include "Common.h"
+#include "QuadEstimatorEKF.h"
+#include "Utility/SimpleConfig.h"
+#include "Utility/StringUtils.h"
+#include "Math/Quaternion.h"
+
+#include <vector>
+#include <iostream>
+
+using namespace SLR;
+
+const int QuadEstimatorEKF::QUAD_EKF_NUM_STATES;
+
+QuadEstimatorEKF::QuadEstimatorEKF(string config, string name)
+  : BaseQuadEstimator(config),
+  Q(QUAD_EKF_NUM_STATES, QUAD_EKF_NUM_STATES),
+  R_GPS(6, 6),
+  R_Mag(1, 1),
+  ekfState(QUAD_EKF_NUM_STATES),
+  ekfCov(QUAD_EKF_NUM_STATES, QUAD_EKF_NUM_STATES),
+  trueError(QUAD_EKF_NUM_STATES)
+{
+  _name = name;
+  Init();
+}
+
+QuadEstimatorEKF::~QuadEstimatorEKF()
+{
+
+}
+
+void QuadEstimatorEKF::Init()
+{
+  ParamsHandle paramSys = SimpleConfig::GetInstance();
+
+  paramSys->GetFloatVector(_config + ".InitState", ekfState);
+
+  VectorXf initStdDevs(QUAD_EKF_NUM_STATES);
+  paramSys->GetFloatVector(_config + ".InitStdDevs", initStdDevs);
+  ekfCov.setIdentity();
+  for (int i = 0; i < QUAD_EKF_NUM_STATES; i++)
+  {
+    ekfCov(i, i) = initStdDevs(i) * initStdDevs(i);
+  }
+
+  // complementary filter params
+  attitudeTau = paramSys->Get(_config + ".AttitudeTau", .1f);
+  dtIMU = paramSys->Get(_config + ".dtIMU", .002f);
+
+  pitchEst = 0;
+  rollEst = 0;
+  
+  // GPS measurement model covariance
+  R_GPS.setZero();
+  R_GPS(0, 0) = R_GPS(1, 1) = powf(paramSys->Get(_config + ".GPSPosXYStd", 0), 2);
+  R_GPS(2, 2) = powf(paramSys->Get(_config + ".GPSPosZStd", 0), 2);
+  R_GPS(3, 3) = R_GPS(4, 4) = powf(paramSys->Get(_config + ".GPSVelXYStd", 0), 2);
+  R_GPS(5, 5) = powf(paramSys->Get(_config + ".GPSVelZStd", 0), 2);
+
+  // magnetometer measurement model covariance
+  R_Mag.setZero();
+  R_Mag(0, 0) = powf(paramSys->Get(_config + ".MagYawStd", 0), 2);
+
+  // load the transition model covariance
+  Q.setZero();
+  Q(0, 0) = Q(1, 1) = powf(paramSys->Get(_config + ".QPosXYStd", 0), 2);
+  Q(2, 2) = powf(paramSys->Get(_config + ".QPosZStd", 0), 2);
+  Q(3, 3) = Q(4, 4) = powf(paramSys->Get(_config + ".QVelXYStd", 0), 2);
+  Q(5, 5) = powf(paramSys->Get(_config + ".QVelZStd", 0), 2);
+  Q(6, 6) = powf(paramSys->Get(_config + ".QYawStd", 0), 2);
+  Q *= dtIMU;
+
+  rollErr = pitchErr = maxEuler = 0;
+  posErrorMag = velErrorMag = 0;
+}
+
+void QuadEstimatorEKF::UpdateFromIMU(V3F accel, V3F gyro)
+{
+  // Improve a complementary filter-type attitude filter
+  // 
+  // Currently a small-angle approximation integration method is implemented
+  // The integrated (predicted) value is then updated in a complementary filter style with attitude information from accelerometers
+  // 
+  // Implement a better integration method that uses the current attitude estimate (rollEst, pitchEst and ekfState(6))
+  // to integrate the body rates into new Euler angles.
+  //
+  // HINTS:
+  //  - there are several ways to go about this, including:
+  //    1) create a rotation matrix based on your current Euler angles, integrate that, convert back to Euler angles
+  //    OR 
+  //    2) use the Quaternion<float> class, which has a handy FromEuler123_RPY function for creating a quaternion from Euler Roll/PitchYaw
+  //       (Quaternion<float> also has a IntegrateBodyRate function, though this uses quaternions, not Euler angles)
+
+  ////////////////////////////// BEGIN STUDENT CODE ///////////////////////////
+
+
+  // SMALL ANGLE GYRO INTEGRATION:
+  // (replace the code below)
+  // make sure you comment it out when you add your own code -- otherwise e.g. you might integrate yaw twice
+
+
+  float predictedPitch = pitchEst + dtIMU * gyro.y;
+  float predictedRoll = rollEst + dtIMU * gyro.x;
+  ekfState(6) = ekfState(6) + dtIMU * gyro.z;	// yaw
+
+  // normalize yaw to -pi .. pi
+  if (ekfState(6) > F_PI) ekfState(6) -= 2.f*F_PI;
+  if (ekfState(6) < -F_PI) ekfState(6) += 2.f*F_PI;
+
+  
+
+  /////////////////////////////// END STUDENT CODE ////////////////////////////
+
+  // CALCULATE UPDATE
+  accelRoll = atan2f(accel.y, accel.z);
+  accelPitch = atan2f(-accel.x, 9.81f);
+
+  // FUSE INTEGRATION AND UPDATE
+  rollEst = attitudeTau / (attitudeTau + dtIMU) * (predictedRoll)+dtIMU / (attitudeTau + dtIMU) * accelRoll;
+  pitchEst = attitudeTau / (attitudeTau + dtIMU) * (predictedPitch)+dtIMU / (attitudeTau + dtIMU) * accelPitch;
+
+  lastGyro = gyro;
+}
+
+void QuadEstimatorEKF::UpdateTrueError(V3F truePos, V3F trueVel, Quaternion<float> trueAtt)
+{
+  VectorXf trueState(QUAD_EKF_NUM_STATES);
+  trueState(0) = truePos.x;
+  trueState(1) = truePos.y;
+  trueState(2) = truePos.z;
+  trueState(3) = trueVel.x;
+  trueState(4) = trueVel.y;
+  trueState(5) = trueVel.z;
+  trueState(6) = trueAtt.Yaw();
+
+  trueError = ekfState - trueState;
+  if (trueError(6) > F_PI) trueError(6) -= 2.f*F_PI;
+  if (trueError(6) < -F_PI) trueError(6) += 2.f*F_PI;
+
+  pitchErr = pitchEst - trueAtt.Pitch();
+  rollErr = rollEst - trueAtt.Roll();
+  maxEuler = MAX(fabs(pitchErr), MAX(fabs(rollErr), fabs(trueError(6))));
+
+  posErrorMag = truePos.dist(V3F(ekfState(0), ekfState(1), ekfState(2)));
+  velErrorMag = trueVel.dist(V3F(ekfState(3), ekfState(4), ekfState(5)));
+}
+
+VectorXf QuadEstimatorEKF::PredictState(VectorXf curState, float dt, V3F accel, V3F gyro)
+{
+  assert(curState.size() == QUAD_EKF_NUM_STATES);
+  VectorXf predictedState = curState;
+  // Predict the current state forward by time dt using current accelerations and body rates as input
+  // INPUTS: 
+  //   curState: starting state
+  //   dt: time step to predict forward by [s]
+  //   accel: acceleration of the vehicle, in body frame, *not including gravity* [m/s2]
+  //   gyro: body rates of the vehicle, in body frame [rad/s]
+  //   
+  // OUTPUT:
+  //   return the predicted state as a vector
+
+  // HINTS 
+  // - dt is the time duration for which you should predict. It will be very short (on the order of 1ms)
+  //   so simplistic integration methods are fine here
+  // - we've created an Attitude Quaternion for you from the current state. Use 
+  //   attitude.Rotate_BtoI(<V3F>) to rotate a vector from body frame to inertial frame
+  // - the yaw integral is already done in the IMU update. Be sure not to integrate it again here
+
+  Quaternion<float> attitude = Quaternion<float>::FromEuler123_RPY(rollEst, pitchEst, curState(6));
+
+  ////////////////////////////// BEGIN STUDENT CODE ///////////////////////////
+
+  // :::: Iterate predictedState() matrix for dt *curState() and dt * acc_w.x, w.y, w.z
+  
+  /* int main ()
+{
+  //creating a vector of integers
+  std::vector<int> vectorInts;  
+  std::cout<<"vectorInts has "<<vectorInts.size()<<" elements\n";
+  
+  //Changing the size of vectorInts to 6
+  vectorInts.resize(6);
+  std::cout<<"\n\nvectorInts now has "<<vectorInts.size()<<" elements\n";
+ 
+  return 0;
+}
+
+  */
+/* :::: Udacity C++ Vectors and Iterators
+
+int main ()
+int main ()
+{
+  //creating a vector of integers
+  std::vector<int> vectorInts;  
+  //creating an iterator for the vector
+  std::vector<int>::iterator it;
+  
+  std::cout<<"vectorInts has "<<vectorInts.size()<<" elements\n";
+  
+  std::cout<<"Adding 23 to the vector\n";
+  vectorInts.assign(1,23);
+  for (it = vectorInts.begin(); it != vectorInts.end(); ++it)
+    std::cout<<*it<<" ";
+  
+
+  std::cout<<"\n\nAdding four elements to the vector\n";
+  //assigning the value 3 to 4 elements of the vector
+  vectorInts.push_back(24);
+  vectorInts.push_back(25);
+  vectorInts.push_back(26);
+  vectorInts.push_back(27);
+  std::cout<<"vectorInts has "<<vectorInts.size()<<" elements\n";
+  
+  //printing the contents of vectorInts
+  std::cout<<"VectorInts has these elements:\n";
+  for (it = vectorInts.begin(); it != vectorInts.end(); ++it)
+    std::cout<<*it<<" ";
+ 
+  return 0;
+}
+
+*/
+
+/* ::::
+ * @file ekf.cpp
+ * Core functions for ekf attitude and position estimator.
+ *
+ * @author Roman Bast <bapstroman@gmail.com>
+ * @author Paul Riseborough <p_riseborough@live.com.au>
+ */
+
+bool Ekf::initialiseTilt()
+{
+	const float accel_norm = _accel_lpf.getState().norm();
+	const float gyro_norm = _gyro_lpf.getState().norm();
+
+	if (accel_norm < 0.8f * CONSTANTS_ONE_G ||
+	    accel_norm > 1.2f * CONSTANTS_ONE_G ||
+	    gyro_norm > math::radians(15.0f)) {
+		return false;
+	}
+
+	// get initial roll and pitch estimate from delta velocity vector, assuming vehicle is static
+	const Vector3f gravity_in_body = _accel_lpf.getState().normalized();
+	const float pitch = asinf(gravity_in_body(0));
+	const float roll = atan2f(-gravity_in_body(1), -gravity_in_body(2));
+
+	_state.quat_nominal = Quatf{Eulerf{roll, pitch, 0.0f}};
+	_R_to_earth = Dcmf(_state.quat_nominal);
+
+	return true;
+}
+
+void Ekf::predictState()
+{
+	// apply imu bias corrections
+	Vector3f corrected_delta_ang = _imu_sample_delayed.delta_ang - _state.delta_ang_bias;
+
+	// subtract component of angular rate due to earth rotation
+	corrected_delta_ang -= _R_to_earth.transpose() * _earth_rate_NED * _imu_sample_delayed.delta_ang_dt;
+
+	const Quatf dq(AxisAnglef{corrected_delta_ang});
+
+	// rotate the previous quaternion by the delta quaternion using a quaternion multiplication
+	_state.quat_nominal = (_state.quat_nominal * dq).normalized();
+
+	_R_to_earth = Dcmf(_state.quat_nominal);
+
+	// Calculate an earth frame delta velocity
+	const Vector3f corrected_delta_vel = _imu_sample_delayed.delta_vel - _state.delta_vel_bias;
+	const Vector3f corrected_delta_vel_ef = _R_to_earth * corrected_delta_vel;
+
+	// calculate a filtered horizontal acceleration with a 1 sec time constant
+	// this are used for manoeuvre detection elsewhere
+	const float alpha = 1.0f - _imu_sample_delayed.delta_vel_dt;
+	_accel_lpf_NE = _accel_lpf_NE * alpha + corrected_delta_vel_ef.xy();
+
+	// save the previous value of velocity so we can use trapzoidal integration
+	const Vector3f vel_last = _state.vel;
+
+	// calculate the increment in velocity using the current orientation
+	_state.vel += corrected_delta_vel_ef;
+
+	// compensate for acceleration due to gravity
+	_state.vel(2) += CONSTANTS_ONE_G * _imu_sample_delayed.delta_vel_dt;
+
+	// predict position states via trapezoidal integration of velocity
+	_state.pos += (vel_last + _state.vel) * _imu_sample_delayed.delta_vel_dt * 0.5f;
+
+	constrainStates();
+
+	// calculate an average filter update time
+	float input = 0.5f * (_imu_sample_delayed.delta_vel_dt + _imu_sample_delayed.delta_ang_dt);
+
+	// filter and limit input between -50% and +100% of nominal value
+	input = math::constrain(input, 0.5f * FILTER_UPDATE_PERIOD_S, 2.0f * FILTER_UPDATE_PERIOD_S);
+	_dt_ekf_avg = 0.99f * _dt_ekf_avg + 0.01f * input;
+
+	// some calculations elsewhere in code require a raw angular rate vector so calculate here to avoid duplication
+	// protect angainst possible small timesteps resulting from timing slip on previous frame that can drive spikes into the rate
+	// due to insufficient averaging
+	if (_imu_sample_delayed.delta_ang_dt > 0.25f * FILTER_UPDATE_PERIOD_S) {
+		_ang_rate_delayed_raw = _imu_sample_delayed.delta_ang / _imu_sample_delayed.delta_ang_dt;
+	}
+
+}
+
+  // ::::
+  // :::: from DQNdrone AirSim 
+
+  /*
+  * Predict the previous quaternion output state forward using the latest IMU delta angle data.
+  */
+Quatf Ekf::calculate_quaternion() const
+{
+	// Correct delta angle data for bias errors using bias state estimates from the EKF and also apply
+	// corrections required to track the EKF quaternion states
+	const Vector3f delta_angle{_newest_high_rate_imu_sample.delta_ang - _state.delta_ang_bias * (_dt_imu_avg / _dt_ekf_avg) + _delta_angle_corr};
+
+	// increment the quaternions using the corrected delta angle vector
+	// the quaternions must always be normalised after modification
+	return Quatf{_output_new.quat_nominal * AxisAnglef{delta_angle}}.unit();
+}
+
+  // ::::
+ 
+  /////////////////////////////// END STUDENT CODE ////////////////////////////
+
+  return predictedState;
